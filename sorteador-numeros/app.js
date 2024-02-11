dda384d730c822642c91fa57458efbe616976173
JasCastro21 <jasminecastr0@edu.unifor.br>
2024-02-11 12:59:47 -0300

    function sortear

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,10 @@
+function sortear(){
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+
+    alert(`Quantidade: ${quantidade}`);
+    alert(`Do Numero: ${de}`);
+    alert(`Ate o numero: ${ate}`);
+
+}

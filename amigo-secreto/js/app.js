8c200ae2e014987ebe4fd305d22934df78e4dce7
JasCastro21 <jasminecastr0@edu.unifor.br>
2024-02-17 20:26:23 -0300

    Embaralhando o array de amigos

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -1,6 +1,10 @@
+let amigos = [];
+
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
     let lista = document.getElementById('lista-amigos');
+
+    amigos.push(amigo.value);
 
     if (lista.textContent.trim() === '') {
         lista.textContent = amigo.value;
@@ -8,4 +12,21 @@ function adicionar() {
         lista.textContent += ', ' + amigo.value;
     }
     amigo.value = '';
+}
+
+function sortear(){
+    embaralha(amigos);
+    
+}
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
 }

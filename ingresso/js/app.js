4b8acc4acc3da3834b88c2a983c8bbcbfbfd2088
JasCastro21 <jasminecastr0@edu.unifor.br>
2024-02-17 19:59:50 -0300

    Realizando compra para outros tipos

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -4,7 +4,11 @@ function comprar(){
 
     if(tipo.value == 'pista'){
         comprarPista(qtd);
-    } 
+    } else if (tipo.value == 'superior'){
+        comprarSuperior(qtd);
+    } else {
+        comprarInferior(qtd);
+    }
 }
 
 function comprarPista(qtd){
@@ -14,6 +18,28 @@ function comprarPista(qtd){
     } else {
         qtdPista = qtdPista - qtd
         document.getElementById('qtd-pista').textContent = qtdPista
+        alert('Compra realizada com sucesso');
+    }
+}
+
+function comprarSuperior(qtd){
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if(qtd > qtdSuperior){
+        alert('Quantida de indisponivel para o tipo superior');
+    } else {
+        qtdSuperior = qtdSuperior - qtd
+        document.getElementById('qtd-superior').textContent = qtdSuperior
+        alert('Compra realizada com sucesso');
+    }
+}
+
+function comprarInferior(qtd){
+    let comprarInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if(qtd > comprarInferior){
+        alert('Quantida de indisponivel para o tipo inferior');
+    } else {
+        comprarInferior = comprarInferior - qtd
+        document.getElementById('qtd-inferior').textContent = comprarInferior
         alert('Compra realizada com sucesso');
     }
 }
